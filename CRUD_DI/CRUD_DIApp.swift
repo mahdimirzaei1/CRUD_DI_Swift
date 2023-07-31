84f28061632ec56067f897c9a1a1206da2b74466
Mahdi <mahdimirzaei1@yahoo.com>
2023-07-31 10:31:54 +0330

    Initial Commit

---
+++ CRUD_DI/CRUD_DIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_DIApp.swift
+//  CRUD_DI
+//
+//  Created by Mahdi Mirzaei on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_DIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
